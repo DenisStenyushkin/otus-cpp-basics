6760282d8cebb2ae4a36a4efd9e05edc7a66a47a
Denis Stenyushkin <denisstenyushkin@yandex.ru>
2024-05-19 15:43:30 +0000

    hw done

--- statistics.cpp
+++ statistics.cpp
@@ -1,5 +1,8 @@
+#include <algorithm>
 #include <iostream>
 #include <limits>
+#include <math.h>
+#include <vector>
 
 class IStatistics {
 public:
@@ -10,9 +13,10 @@ public:
 	virtual const char * name() const = 0;
 };
 
+
 class Min : public IStatistics {
 public:
-	Min() : m_min{std::numeric_limits<double>::min()} {
+	Min() : m_min{std::numeric_limits<double>::max()} {
 	}
 
 	void update(double next) override {
@@ -33,17 +37,112 @@ private:
 	double m_min;
 };
 
+class Max : public IStatistics {
+public:
+	Max() : m_max{std::numeric_limits<double>::min()} {
+	}
+
+	void update(double next) override {
+		if (next > m_max) {
+			m_max = next;
+		}
+	}
+
+	double eval() const override {
+		return m_max;
+	}
+
+	const char * name() const override {
+		return "max";
+	}
+
+private:
+	double m_max;
+};
+
+class Mean : public IStatistics {
+public:
+	void update(double next) override {
+		m_sum += next;
+		++cnt;
+	}
+
+	double eval() const override {
+		return m_sum / cnt;
+	}
+
+	const char* name() const override {
+		return "mean";
+	}
+
+private:
+	double m_sum;
+	size_t cnt;
+};
+
+class StdDev : public IStatistics {
+public:
+	void update(double next) override {
+		m_values.push_back(next);
+		m_sum += next;
+	}
+
+	double eval() const override {
+		double sum_squares {0};
+		double avg = mean();
+
+		for (auto e : m_values) {
+			sum_squares += std::pow((e - avg), 2);
+		}
+
+		return std::sqrt( sum_squares / (m_values.size() - 1));
+	}
+
+	const char* name() const override {
+		return "std";
+	}
+
+private:
+	std::vector<double> m_values;
+	double m_sum;
+
+	double mean() const {
+		return m_sum / m_values.size();
+	}
+};
+
+class Pct90 : public IStatistics {
+public:
+	void update(double next) override {
+		m_values.push_back(next);
+	}
+
+	double eval() const override {
+		std::sort(m_values.begin(), m_values.end());
+		int k = static_cast<int>( 0.9 * (m_values.size() - 1) );
+		double an = 0.9 * m_values.size();
+		
+		if (k + 1 == static_cast<int>(an)) return (m_values[k] + m_values[k+1]) / 2;
+		else if (k + 1 < an) return m_values[k+1];
+		else return m_values[k];
+	}
+
+	const char* name() const override {
+		return "pct90";
+	}
+
+private:
+	mutable std::vector<double> m_values;
+};
+
+
 int main() {
-
-	const size_t statistics_count = 1;
-	IStatistics *statistics[statistics_count];
-
-	statistics[0] = new Min{};
+	std::vector<IStatistics*> statistics{ new Min{}, new Max{}, new Mean{}, new StdDev{}, new Pct90{} };
 
 	double val = 0;
 	while (std::cin >> val) {
-		for (size_t i = 0; i < statistics_count; ++i) {
-			statistics[i]->update(val);
+		for (auto & s : statistics) {
+			s->update(val);
 		}
 	}
 
@@ -54,13 +153,13 @@ int main() {
 	}
 
 	// Print results if any
-	for (size_t i = 0; i < statistics_count; ++i) {
-		std::cout << statistics[i]->name() << " = " << statistics[i]->eval() << std::endl;
+	for (const auto & s : statistics) {
+		std::cout << s->name() << " = " << s->eval() << std::endl;
 	}
 
 	// Clear memory - delete all objects created by new
-	for (size_t i = 0; i < statistics_count; ++i) {
-		delete statistics[i];
+	for (auto & s : statistics) {
+		delete s;
 	}
 
 	return 0;
